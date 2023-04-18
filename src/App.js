a107dba013d2a0ff8ebb94c98b58b043d677f943
MuhammedAliDilek <muhammed202-42@hotmail.com>
2023-04-18 18:03:46 +0200

    store activities on local storage

--- src/App.js
+++ src/App.js
@@ -2,9 +2,9 @@ import "./App.css";
 import Form from "./components/Form/Form";
 import { useState } from "react";
 import { uid } from "uid";
-
+import useLocalStorageState from "use-local-storage-state"
 function App() {
-  const [activities, setActivities] = useState([{ name: "ert" }]);
+  const [activities, setActivities] =useLocalStorageState ("activities", {defaultValue:  []});
 
   function handleAddActivity(newActivity) {
     setActivities([{ id: uid(), ...newActivity }, ...activities]);
---
+++ src/components/Form/List/List.js
@@ -0,0 +1,8 @@
+
+function List({activities}) {
+  return (
+    <div>List</div>
+  )
+}
+
+export default List
