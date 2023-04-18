39b52d606de56894206fbe442129ccfdae7ccb14
= <=>
2023-04-18 19:35:27 +0200

    Filter activities according to good and bad weather

--- src/App.js
+++ src/App.js
@@ -2,14 +2,17 @@ import "./App.css";
 import Form from "./components/Form/Form";
 import { useState } from "react";
 import { uid } from "uid";
-import useLocalStorageState from "use-local-storage-state"
+import useLocalStorageState from "use-local-storage-state";
 import List from "./components/List/List";
 function App() {
-  const [activities, setActivities] =useLocalStorageState ("activities", {defaultValue:  []});
-  
+  const [activities, setActivities] = useLocalStorageState("activities", {
+    defaultValue: [],
+  });
+  const isGoodWeather = true;
+
   function clearLocalStorage() {
-    setActivities([])
-    localStorage.clear()
+    setActivities([]);
+    localStorage.clear();
   }
   function handleAddActivity(newActivity) {
     setActivities([{ id: uid(), ...newActivity }, ...activities]);
@@ -18,10 +21,13 @@ function App() {
 
   return (
     <div className="App">
-      <Form onAddActivity={handleAddActivity} /> 
-      <List activities={activities}/> 
-      <button type="button" onClick={clearLocalStorage}> Clear Local Storage and Activities</button> 
-
+      <Form onAddActivity={handleAddActivity} />
+      {isGoodWeather ? <p>Good weather</p> : <p>Bad weather</p>}
+      <List activities={activities} isGoodWeather={isGoodWeather} />
+      <button type="button" onClick={clearLocalStorage}>
+        {" "}
+        Clear Local Storage and Activities
+      </button>
     </div>
   );
 }
