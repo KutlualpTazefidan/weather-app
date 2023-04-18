b18745aefea09559405f9f1db0bdbddcfa604cb8
= <=>
2023-04-18 17:29:39 +0200

    Input and store activities

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import "./App.css";
 import Form from "./components/Form/Form";
+import { useState } from "react";
+import { uid } from "uid";
 
 function App() {
+  const [activities, setActivities] = useState([{ name: "ert" }]);
+
+  function handleAddActivity(newActivity) {
+    setActivities([{ id: uid(), ...newActivity }, ...activities]);
+  }
+
   return (
     <div className="App">
-      <Form />
+      <Form onAddActivity={handleAddActivity} />
     </div>
   );
 }
--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,19 +1,24 @@
-function Form(onAddActivity) {
+function Form({ onAddActivity }) {
+  function onSubmitForm(e) {
+    e.preventDefault();
+    const formData = new FormData(e.target);
+    const data = Object.fromEntries(formData);
+    onAddActivity(data);
+  }
   return (
-    <form>
+    <form onSubmit={onSubmitForm}>
       <h1>Add new Activity</h1>
       <label htmlFor="inputName">Name: </label>
-      <input type="text" id="inputName" /> <br />
+      <input type="text" id="inputName" name="inputField" /> <br />
       <label htmlFor="goodWeatherCheckBox">Good-weather activity: </label>
-      <input type="checkbox" id="goodWeatherCheckBox" /> <br />
-      <button
-        type="button"
+      <input type="checkbox" id="goodWeatherCheckBox" name="checkboxField" />
+      <br />
+      <input
+        type="submit"
         className="new-activity-submit-button"
         aria-label="new activity submit button"
-        onClick={onAddActivity}
-      >
-        Submit
-      </button>
+        value="Submit"
+      />
     </form>
   );
 }
