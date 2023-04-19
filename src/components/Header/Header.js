7baef5aac4244f497c473b1023c2e4ab0de91030
= <=>
2023-04-19 23:02:30 +0200

    Fix margin of the activity card

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,12 +4,12 @@ function Header({ weather }) {
     ? "The weather is awesome! Go outside and:"
     : "Bad weather outside! Here is what you can do now";
   return (
-    <>
+    <div>
       <div className="weather-status-bar">
         {weather.condition} {weather.temperature} °C
       </div>
       <p className="weather-message">{weatherMessage}</p>
-    </>
+    </div>
   );
 }
 
